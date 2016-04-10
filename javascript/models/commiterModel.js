1e1e5efe7f19c6b28f42b15025bf5f329718a520
Tomer David <younrg@gmail.com>
2016-04-10 10:27:23 +0200

    changed to work with parse

--- javascript/models/commiterModel.js
+++ javascript/models/commiterModel.js
@@ -2,5 +2,12 @@ var CommiterModel = Backbone.Model.extend({
   defaults: {
     name: '',
     img: ''
+  },
+
+  parse: function(res){
+    return {
+      name: res.committer.login,
+      img: res.committer.avatar_url
+    };
   }
 });
--- javascript/views/commiterView.js
+++ javascript/views/commiterView.js
@@ -3,12 +3,14 @@ var CommiterView = Backbone.View.extend({
   template: Handlebars.compile($('#commiter-template').html()),
 
   render: function(){
-    var obj = {
-      name: this.model.toJSON().committer.login,
-      img: this.model.toJSON().committer.avatar_url
-    }
-    this.$el.html(this.template(obj));
+    // var obj = {
+    //   name: this.model.toJSON().committer.login,
+    //   img: this.model.toJSON().committer.avatar_url
+    // }
+    this.$el.html(this.template(this.model.toJSON()));
 
     return this;
   }
+
+
 });
